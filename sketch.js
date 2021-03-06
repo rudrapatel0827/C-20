0c607be919009c42163f9e81d747cc787cc09f07
rudrapatel0827 <79108865+rudrapatel0827@users.noreply.github.com>
2021-03-06 12:31:42 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var a,b
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50); 
+  a.shapeColor="red"
+  b=createSprite(200,100,20,100);
+  b.shapeColor="red"
+}
+
+function draw() {
+  background("blue");  
+  a.x= World.mouseX
+  a.y=World.mouseY
+  if(a.x-b.x<a.width/2+b.width/2
+    &&b.x-a.x<a.width/2+b.width/2
+    &&a.y-b.y<a.height/2+b.height/2
+    &&b.y-a.y<a.height/2+b.height/2){
+    a.shapeColor="green"
+    b.shapeColor="green"
+  }
+  else{
+    a.shapeColor="red"
+    b.shapeColor="red"
+  }
+  drawSprites();
+}
